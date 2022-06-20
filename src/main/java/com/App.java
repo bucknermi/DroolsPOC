5a5fb0a4137f4118a485e69810f362523ac1c081
Build-coder <59242259+Build-coder@users.noreply.github.com>
2022-06-20 14:29:46 -0400

    not sure what to do to get salary to save to h2

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -4,8 +4,10 @@ import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
+@EnableJpaRepositories(basePackages = "com.repo") 
 public class App {
 
 	public static void main(String[] args) {
--- src/main/java/com/controller/SalaryController.java
+++ src/main/java/com/controller/SalaryController.java
@@ -1,8 +1,11 @@
 package com.controller;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.model.Employee;
@@ -35,4 +38,6 @@ public class SalaryController {
 
 		return salary;
 	}
+
+
 }
